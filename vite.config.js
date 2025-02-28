9b0ae87459e97036258ab705a7811432b15b7885
sethbr11 <sethabrock@gmail.com>
2025-02-28 15:42:08 -0700

    Added images/ directory to build process

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,22 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/images",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -14,5 +26,10 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL("./index.html", import.meta.url)),
+      },
+    },
   },
 });
